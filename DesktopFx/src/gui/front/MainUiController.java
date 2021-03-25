b92d473c537494d28b53002470df17e9806cf9dd
hsan256 <47532941+hsan256@users.noreply.github.com>
2021-03-25 14:25:15 +0100

    +front

--- DesktopFx/src/gui/front/MainUiController.java
+++ DesktopFx/src/gui/front/MainUiController.java
@@ -7,20 +7,32 @@ package gui.front;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.event.ActionEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
+import javafx.geometry.Insets;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
+import javafx.util.Duration;
 import models.Menu;
+import models.Regime;
+import services.ServiceMenu;
+import services.ServiceRegime;
+import tray.notification.NotificationType;
+import tray.notification.TrayNotification;
+import utils.DataSource;
 
 /**
  * FXML Controller class
@@ -28,28 +40,91 @@ import models.Menu;
  * @author trabe
  */
 public class MainUiController implements Initializable {
-
+    
+    ObservableList<String> list = FXCollections.observableArrayList();
+    
+    Connection cnx = DataSource.getInstance().getCnx();
+    
+    ObservableList<Menu> listMenu = FXCollections.observableArrayList();
+    
     @FXML
-    private Pane contentArea;
-    private List<Menu> fruits = new ArrayList<>();
+    private ComboBox<String> combo;
+    @FXML
+    private GridPane grid;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        load_nom();
+    }
+    
+    public void load_nom(){
         try {
-            show();
-        } catch (IOException ex) {
-            Logger.getLogger(MainUiController.class.getName()).log(Level.SEVERE, null, ex);
+            String requete = "SELECT type FROM regime";
+            Statement pst = cnx.prepareStatement(requete);
+            ResultSet rs = pst.executeQuery(requete);
+            while (rs.next())
+            {
+                list.add(rs.getString("type"));
+            }
+            
+            combo.setItems(list);
+        } catch(SQLException ex) {
+            System.err.println(ex.getMessage());
         }
     }
     
-
-    public void show() throws IOException{
-        
-        Parent fxml = FXMLLoader.load(getClass().getResource("/gui/front/MenuFront.fxml"));
-        contentArea.getChildren().removeAll();
-        contentArea.getChildren().setAll(fxml);
+    @FXML
+    private void load(){
+        try {
+            grid.getChildren().clear();
+            String reg = (String)combo.getValue();
+            String requete = "SELECT id_regime FROM regime WHERE type="+(char)34+reg+(char)34;
+            Statement pst = cnx.prepareStatement(requete);
+            ResultSet rs = pst.executeQuery(requete);
+            
+            if(rs.next()){
+                ServiceMenu SP = new ServiceMenu();
+                listMenu = SP.afficherFront(rs.getInt(1));
+                int column = 1;
+                int row = 0;
+                
+                try {
+                    for (int i = 0; i < listMenu.size(); i++) {
+                        FXMLLoader fxmlLoader = new FXMLLoader();
+                        fxmlLoader.setLocation(getClass().getResource("/gui/front/MenuFront.fxml"));
+                        Pane Pane = fxmlLoader.load();
+                        
+                        MenuFrontController itemController = fxmlLoader.getController();
+                        itemController.setData(listMenu.get(i));
+                        
+                        if (column > 1) {
+                            column = 1;
+                            row++;
+                        }
+                        
+                        grid.add(Pane, column, row); //(child,column,row)
+                        //set grid width
+                        grid.setMinWidth(Region.USE_COMPUTED_SIZE);
+                        grid.setPrefWidth(Region.USE_COMPUTED_SIZE);
+                        grid.setMaxWidth(Region.USE_PREF_SIZE);
+                        
+                        //set grid height
+                        grid.setMinHeight(Region.USE_COMPUTED_SIZE);
+                        grid.setPrefHeight(Region.USE_COMPUTED_SIZE);
+                        grid.setMaxHeight(Region.USE_PREF_SIZE);
+                        
+                        GridPane.setMargin(Pane, new Insets(10));
+                        column++;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(MainUiController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- DesktopFx/src/gui/front/MenuFrontController.java
+++ DesktopFx/src/gui/front/MenuFrontController.java
@@ -5,18 +5,22 @@
  */
 package gui.front;
 
+import java.io.IOException;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import models.Menu;
 import utils.DataSource;
 
 /**
@@ -46,37 +50,39 @@ public class MenuFrontController implements Initializable {
     private Label Menu_Jour_Num;
     @FXML
     private Label total_calories;
+    
+    private Menu Menu = new Menu();
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        Connection cnx = DataSource.getInstance().getCnx();
-        try {
-            String requete = "SELECT * FROM menu";
-            Statement pst = cnx.prepareStatement(requete);
-            ResultSet rs = pst.executeQuery(requete);
-            
-            while (rs.next()) {
-                Description.setText(rs.getString(2));
-                Menu_Jour_Num.setText("Menu Jour " + rs.getInt(3));
-                matin.setText(rs.getString(4));
-                img_matin.setImage(new Image("file:/C:/Users/trabe/Desktop/DesktopFx/src/images/" + rs.getString(5)));
-                dejeuner.setText(rs.getString(6));
-                img_dejeuner.setImage(new Image("file:/C:/Users/trabe/Desktop/DesktopFx/src/images/" + rs.getString(7)));
-                dinner.setText(rs.getString(8));
-                img_dinner.setImage(new Image("file:/C:/Users/trabe/Desktop/DesktopFx/src/images/" + rs.getString(9)));
-                total_calories.setText("Total Calorique: " + rs.getString(10));
-//                Image image = new Image(getClass().getResourceAsStream(fruit.getImgSrc()));
-//                img_matin.setImage(image);
-//                Description.setStyle("-fx-background-color: #" + fruit.getColor() + ";\n" +
-//                        "    -fx-background-radius: 30;");            
-            }
-            
-        } catch(SQLException ex) {
-            System.err.println(ex.getMessage());
-        }
-    }    
-    
+    }
+
+
+    public void setData(Menu menu) {
+        this.Menu = menu;
+
+        Description.setText(menu.getDescirption());
+        Menu_Jour_Num.setText("Menu Jour " + menu.getNum_jour());
+        matin.setText(menu.getMatin());
+        img_matin.setImage(new Image("file:/C:/Users/trabe/Desktop/Integration/src/images/" + menu.getMatin_img()));
+        dejeuner.setText(menu.getDejeuner());
+        img_dejeuner.setImage(new Image("file:/C:/Users/trabe/Desktop/Integration/src/images/" + menu.getDejeuner_img()));
+        dinner.setText(menu.getDinner());
+        img_dinner.setImage(new Image("file:/C:/Users/trabe/Desktop/Integration/src/images/" + menu.getDinner_img()));
+        total_calories.setText("Total Calorique: " + menu.getTotal_calories());
+    }
+
+    @FXML
+    private void back(MouseEvent event) throws IOException {
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/front/MainUi.fxml"));/* Exception */
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
